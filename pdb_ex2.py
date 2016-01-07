05de130da5aedbce2f5ad04cd1b3084ff5840ed4
James <jafland@gmail.com>
2016-01-07 06:56:39 -0500

    added postmortem

--- pdb_ex2.py
+++ pdb_ex2.py
@@ -4,12 +4,13 @@ import pdb
 
 def add_one(num):
     result = num + 1
-    print result
+    print(result)
     return result
 
+#exercise used xrange on line13 which was replaced with range in python 3
 def main():
     pdb.set_trace()
-    for num in xrange(0,10):
+    for num in range(0,10):
         add_one(num)
 
 
